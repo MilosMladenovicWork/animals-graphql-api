9ccc196c02fadbeeec8d169256bcee5a717ea9eb
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2024-01-03 23:21:43 +0100

    fix(index): :bug: fix typo

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,4 @@ app.all('/graphql', createHandler({ schema: executableSchema }));
 const port = 3000;
 
 app.listen({ port });
-console.log(`Listening to port: ${port}`);
+console.log(`Listening on port: ${port}`);
