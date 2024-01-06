882de57d32319b31e7f573a381ae158417773a2d
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2024-01-06 17:32:38 +0100

    feat(animal.repository): :sparkles: create class for animal data manipulation

---
+++ src/animals/repository/animal.repository.ts
@@ -0,0 +1,8 @@
+class AnimalRepository {
+  private animals = [
+    { id: 1, name: 'Cat', dateCreated: 1704398763 },
+    { id: 2, name: 'Dog', dateCreated: 1704398763 },
+  ];
+}
+
+export const animalRepository = new AnimalRepository();
