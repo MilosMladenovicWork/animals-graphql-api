f95a478b388b6a740b24d407e434c8b1b858c807
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2024-01-06 18:19:29 +0100

    test(delete-animal): :test_tube: test if deleteOne method is called once and with correct arguments

--- src/animals/resolvers/delete-animal/delete-animal.resolver.spec.ts
+++ src/animals/resolvers/delete-animal/delete-animal.resolver.spec.ts
@@ -2,6 +2,22 @@ import { animalRepository } from '../../repository/animal.repository';
 import { deleteAnimalResolver } from './delete-animal.resolver';
 
 describe('Test delete-animal.resolver', () => {
+  it('should call deleteOne repository method once', () => {
+    const deleteOneMethod = jest.spyOn(animalRepository, 'deleteOne');
+
+    deleteAnimalResolver(undefined, { id: 1 });
+
+    expect(deleteOneMethod).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call deleteOne repository without arguments', () => {
+    const deleteOneMethod = jest.spyOn(animalRepository, 'deleteOne');
+
+    deleteAnimalResolver(undefined, { id: 1 });
+
+    expect(deleteOneMethod).toHaveBeenCalledWith({ id: 1 });
+  });
+
   it('should return deleted animal', () => {
     const animal = { id: 1, name: 'Animal1', dateCreated: 123 };
 
