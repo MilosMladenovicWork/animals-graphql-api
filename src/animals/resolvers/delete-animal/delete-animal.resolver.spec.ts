40827b1a66b7deeec6af2d08765edb13e145ad4d
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2024-01-06 18:09:01 +0100

    test(delete-animal): :test_tube: use mock for deleteOne method

--- src/animals/resolvers/delete-animal/delete-animal.resolver.spec.ts
+++ src/animals/resolvers/delete-animal/delete-animal.resolver.spec.ts
@@ -1,16 +1,25 @@
+import { animalRepository } from '../../repository/animal.repository';
 import { deleteAnimalResolver } from './delete-animal.resolver';
 
 describe('Test delete-animal.resolver', () => {
   it('should return deleted animal', () => {
+    const animal = { id: 1, name: 'Animal1', dateCreated: 123 };
+
+    jest.spyOn(animalRepository, 'deleteOne').mockReturnValue(animal);
+
     const data = deleteAnimalResolver(undefined, { id: 1 });
 
-    expect(data).toStrictEqual({ dateCreated: 1704398763, id: 1, name: 'Cat' });
+    expect(data).toEqual(animal);
   });
 
   it('should return null when animal is not found', () => {
+    const animal = null;
+
+    jest.spyOn(animalRepository, 'deleteOne').mockReturnValue(animal);
+
     const data = deleteAnimalResolver(undefined, { id: 10000000 });
 
-    expect(data).toEqual(null);
+    expect(data).toEqual(animal);
   });
 
   // TODO: test if actual data has changed after deletion
