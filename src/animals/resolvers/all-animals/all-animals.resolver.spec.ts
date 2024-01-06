106b19ccaaefec5924c85f99dcd37e8e3d753722
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2024-01-06 18:12:56 +0100

    test(all-animals): :test_tube: check if findAll is called once

--- src/animals/resolvers/all-animals/all-animals.resolver.spec.ts
+++ src/animals/resolvers/all-animals/all-animals.resolver.spec.ts
@@ -2,6 +2,14 @@ import { animalRepository } from '../../repository/animal.repository';
 import { allAnimalsResolver } from './all-animals.resolver';
 
 describe('Test all-animals.resolver', () => {
+  it('should call findAll repository method once', () => {
+    const findAllMethod = jest.spyOn(animalRepository, 'findAll');
+
+    allAnimalsResolver();
+
+    expect(findAllMethod).toHaveBeenCalledTimes(1);
+  });
+
   it('should return all animals', () => {
     const animals = [
       { id: 1, name: 'Animal1', dateCreated: 123 },
