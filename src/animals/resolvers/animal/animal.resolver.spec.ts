59cf1f7e7a9ca45edaca651c7c499c16aec7622b
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2024-01-06 18:06:40 +0100

    test(animal): :test_tube: use mock for findOne method call

--- src/animals/resolvers/animal/animal.resolver.spec.ts
+++ src/animals/resolvers/animal/animal.resolver.spec.ts
@@ -3,14 +3,22 @@ import { animalResolver } from './animal.resolver';
 
 describe('Test animal.resolver', () => {
   it('should return correct animal when id is supplied', () => {
+    const animal = { id: 2, name: 'Animal2', dateCreated: 123 };
+
+    jest.spyOn(animalRepository, 'findOne').mockReturnValue(animal);
+
     const data = animalResolver(undefined, { id: 2 });
 
-    expect(data).toBe(animalRepository.findOne({ id: 2 }));
+    expect(data).toBe(animal);
   });
 
   it('should return null when non existent id is supplied', () => {
+    const animal = null;
+
+    jest.spyOn(animalRepository, 'findOne').mockReturnValue(animal);
+
     const data = animalResolver(undefined, { id: 100000000 });
 
-    expect(data).toBe(null);
+    expect(data).toBe(animal);
   });
 });
