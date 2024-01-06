95bbfb55a37ec12ebbd2dbb943db76e42cc7d7c2
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2024-01-06 18:15:58 +0100

    test(animal): :test_tube: test if findOne method is called once and with correct argument

--- src/animals/resolvers/animal/animal.resolver.spec.ts
+++ src/animals/resolvers/animal/animal.resolver.spec.ts
@@ -2,6 +2,22 @@ import { animalRepository } from '../../repository/animal.repository';
 import { animalResolver } from './animal.resolver';
 
 describe('Test animal.resolver', () => {
+  it('should call findOne method once', () => {
+    const findOneMethod = jest.spyOn(animalRepository, 'findOne');
+
+    animalResolver(undefined, { id: 2 });
+
+    expect(findOneMethod).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call findOne method with id passed from resolver', () => {
+    const findOneMethod = jest.spyOn(animalRepository, 'findOne');
+
+    animalResolver(undefined, { id: 2 });
+
+    expect(findOneMethod).toHaveBeenCalledWith({ id: 2 });
+  });
+
   it('should return correct animal when id is supplied', () => {
     const animal = { id: 2, name: 'Animal2', dateCreated: 123 };
 
