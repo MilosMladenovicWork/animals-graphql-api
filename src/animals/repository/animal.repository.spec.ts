68188f9e386ca7edbfac2a4ebc481f219d5b1c2d
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2024-01-08 19:46:02 +0100

    refactor(animal.repository.spec): :recycle: organize tests in groups by methods

--- src/animals/repository/animal.repository.spec.ts
+++ src/animals/repository/animal.repository.spec.ts
@@ -2,220 +2,234 @@ import * as convertJsDateToUnix from '../../common/util/convert-js-date-to-unix'
 import { AnimalRepository } from './animal.repository';
 
 describe('Test animal.repository', () => {
-  it('findAll should return all animals', () => {
-    const animalRepository = new AnimalRepository();
+  describe('findAll', () => {
+    it('findAll should return all animals', () => {
+      const animalRepository = new AnimalRepository();
 
-    expect(animalRepository.findAll()).toEqual([]);
-  });
-
-  it('findOne should return null when animal is not found', () => {
-    const animalRepository = new AnimalRepository();
-
-    expect(animalRepository.findOne({ id: 1234 })).toEqual(null);
-  });
-
-  it('findOne should return animal when animal is found', () => {
-    const animalRepository = new AnimalRepository();
-
-    const animal = { id: 1234, name: 'Animal', dateCreated: 1234 };
-
-    animalRepository.save({ animal });
-
-    expect(animalRepository.findOne({ id: animal.id })).toEqual(animal);
-  });
-
-  it('create should create animal', () => {
-    const animalRepository = new AnimalRepository();
-
-    const animalName = 'Animal';
-
-    jest
-      .spyOn(convertJsDateToUnix, 'convertJsDateToUnix')
-      .mockReturnValue(1234);
-
-    expect(animalRepository.create({ name: animalName })).toEqual({
-      id: 1,
-      name: animalName,
-      dateCreated: 1234,
+      expect(animalRepository.findAll()).toEqual([]);
     });
   });
 
-  it('create should call convertJsDateToUnix once', () => {
-    const animalRepository = new AnimalRepository();
+  describe('findOne', () => {
+    it('findOne should return null when animal is not found', () => {
+      const animalRepository = new AnimalRepository();
 
-    const animalName = 'Animal';
-
-    const convertJsDateToUnixMock = jest
-      .spyOn(convertJsDateToUnix, 'convertJsDateToUnix')
-      .mockReturnValue(1234);
-
-    animalRepository.create({ name: animalName });
-
-    // 2 because spyOn calls it also
-    expect(convertJsDateToUnixMock).toHaveBeenCalledTimes(2);
-  });
-
-  it('create should generate correct ids', () => {
-    const animalRepository = new AnimalRepository();
-
-    const animal1 = animalRepository.create({ name: 'Animal1' });
-
-    animalRepository.save({ animal: animal1 });
-
-    expect(animal1.id).toBe(1);
-
-    const animal2 = animalRepository.create({ name: 'Animal2' });
-
-    expect(animal2.id).toBe(2);
-  });
-
-  it('save should return saved animal', () => {
-    const animalRepository = new AnimalRepository();
-
-    const animal = { id: 123, name: 'Animal', dateCreated: 1234 };
-
-    const returnValue = animalRepository.save({ animal });
-
-    expect(returnValue).toEqual(animal);
-  });
-
-  it('save should save animal', () => {
-    const animalRepository = new AnimalRepository();
-
-    const animal = { id: 123, name: 'Animal', dateCreated: 1234 };
-
-    const returnValue = animalRepository.save({ animal });
-
-    expect(returnValue).toEqual(animalRepository.getAnimals()[0]);
-  });
-
-  it('save should save animals in correct order', () => {
-    const animalRepository = new AnimalRepository();
-
-    const animal1 = { id: 1, name: 'Animal1', dateCreated: 1234 };
-
-    const animal2 = { id: 1, name: 'Animal1', dateCreated: 1234 };
-
-    animalRepository.save({ animal: animal1 });
-
-    animalRepository.save({ animal: animal2 });
-
-    expect(animalRepository.getAnimals()).toEqual([animal1, animal2]);
-  });
-
-  it('deleteOne should return null if animal is not found', () => {
-    const animalRepository = new AnimalRepository();
-
-    const deletedAnimal = animalRepository.deleteOne({ id: 1234 });
-
-    expect(deletedAnimal).toEqual(null);
-  });
-
-  it('deleteOne should return deleted animal', () => {
-    const animalRepository = new AnimalRepository();
-
-    const animal = { id: 1234, name: 'Animal', dateCreated: 123 };
-
-    animalRepository.save({
-      animal,
+      expect(animalRepository.findOne({ id: 1234 })).toEqual(null);
     });
 
-    const deletedAnimal = animalRepository.deleteOne({ id: animal.id });
+    it('findOne should return animal when animal is found', () => {
+      const animalRepository = new AnimalRepository();
 
-    expect(deletedAnimal).toEqual(animal);
+      const animal = { id: 1234, name: 'Animal', dateCreated: 1234 };
+
+      animalRepository.save({ animal });
+
+      expect(animalRepository.findOne({ id: animal.id })).toEqual(animal);
+    });
   });
 
-  it('deleteOne should delete animal and don`t change order', () => {
-    const animalRepository = new AnimalRepository();
+  describe('create', () => {
+    it('create should create animal', () => {
+      const animalRepository = new AnimalRepository();
 
-    const animal1 = { id: 1, name: 'Animal1', dateCreated: 123 };
+      const animalName = 'Animal';
 
-    animalRepository.save({
-      animal: animal1,
+      jest
+        .spyOn(convertJsDateToUnix, 'convertJsDateToUnix')
+        .mockReturnValue(1234);
+
+      expect(animalRepository.create({ name: animalName })).toEqual({
+        id: 1,
+        name: animalName,
+        dateCreated: 1234,
+      });
     });
 
-    const animal2 = { id: 2, name: 'Animal2', dateCreated: 123 };
+    it('create should call convertJsDateToUnix once', () => {
+      const animalRepository = new AnimalRepository();
 
-    animalRepository.save({
-      animal: animal2,
+      const animalName = 'Animal';
+
+      const convertJsDateToUnixMock = jest
+        .spyOn(convertJsDateToUnix, 'convertJsDateToUnix')
+        .mockReturnValue(1234);
+
+      animalRepository.create({ name: animalName });
+
+      // 2 because spyOn calls it also
+      expect(convertJsDateToUnixMock).toHaveBeenCalledTimes(2);
     });
 
-    const animal3 = { id: 3, name: 'Animal3', dateCreated: 123 };
+    it('create should generate correct ids', () => {
+      const animalRepository = new AnimalRepository();
 
-    animalRepository.save({
-      animal: animal3,
+      const animal1 = animalRepository.create({ name: 'Animal1' });
+
+      animalRepository.save({ animal: animal1 });
+
+      expect(animal1.id).toBe(1);
+
+      const animal2 = animalRepository.create({ name: 'Animal2' });
+
+      expect(animal2.id).toBe(2);
     });
-
-    animalRepository.deleteOne({ id: animal2.id });
-
-    expect(animalRepository.getAnimals()).toEqual([animal1, animal3]);
   });
 
-  it('updateOne should return null if animal is not found', () => {
-    const animalRepository = new AnimalRepository();
+  describe('save', () => {
+    it('save should return saved animal', () => {
+      const animalRepository = new AnimalRepository();
 
-    animalRepository.save({
-      animal: { id: 1, name: 'Animal', dateCreated: 123 },
+      const animal = { id: 123, name: 'Animal', dateCreated: 1234 };
+
+      const returnValue = animalRepository.save({ animal });
+
+      expect(returnValue).toEqual(animal);
     });
 
-    const returnValue = animalRepository.updateOne(
-      { id: 1234 },
-      { name: 'Animal' }
-    );
+    it('save should save animal', () => {
+      const animalRepository = new AnimalRepository();
 
-    expect(returnValue).toEqual(null);
+      const animal = { id: 123, name: 'Animal', dateCreated: 1234 };
+
+      const returnValue = animalRepository.save({ animal });
+
+      expect(returnValue).toEqual(animalRepository.getAnimals()[0]);
+    });
+
+    it('save should save animals in correct order', () => {
+      const animalRepository = new AnimalRepository();
+
+      const animal1 = { id: 1, name: 'Animal1', dateCreated: 1234 };
+
+      const animal2 = { id: 1, name: 'Animal1', dateCreated: 1234 };
+
+      animalRepository.save({ animal: animal1 });
+
+      animalRepository.save({ animal: animal2 });
+
+      expect(animalRepository.getAnimals()).toEqual([animal1, animal2]);
+    });
   });
 
-  it('updateOne should return null if name is undefined', () => {
-    const animalRepository = new AnimalRepository();
+  describe('deleteOne', () => {
+    it('deleteOne should return null if animal is not found', () => {
+      const animalRepository = new AnimalRepository();
 
-    animalRepository.save({
-      animal: { id: 1, name: 'Animal', dateCreated: 123 },
+      const deletedAnimal = animalRepository.deleteOne({ id: 1234 });
+
+      expect(deletedAnimal).toEqual(null);
     });
 
-    const returnValue = animalRepository.updateOne(
-      { id: 1 },
-      { name: undefined }
-    );
+    it('deleteOne should return deleted animal', () => {
+      const animalRepository = new AnimalRepository();
 
-    expect(returnValue).toEqual(null);
+      const animal = { id: 1234, name: 'Animal', dateCreated: 123 };
+
+      animalRepository.save({
+        animal,
+      });
+
+      const deletedAnimal = animalRepository.deleteOne({ id: animal.id });
+
+      expect(deletedAnimal).toEqual(animal);
+    });
+
+    it('deleteOne should delete animal and don`t change order', () => {
+      const animalRepository = new AnimalRepository();
+
+      const animal1 = { id: 1, name: 'Animal1', dateCreated: 123 };
+
+      animalRepository.save({
+        animal: animal1,
+      });
+
+      const animal2 = { id: 2, name: 'Animal2', dateCreated: 123 };
+
+      animalRepository.save({
+        animal: animal2,
+      });
+
+      const animal3 = { id: 3, name: 'Animal3', dateCreated: 123 };
+
+      animalRepository.save({
+        animal: animal3,
+      });
+
+      animalRepository.deleteOne({ id: animal2.id });
+
+      expect(animalRepository.getAnimals()).toEqual([animal1, animal3]);
+    });
   });
 
-  it('updateOne should update animal and preserve order', () => {
-    const animalRepository = new AnimalRepository();
+  describe('updateOne', () => {
+    it('updateOne should return null if animal is not found', () => {
+      const animalRepository = new AnimalRepository();
 
-    const animal1 = { id: 1, name: 'Animal1', dateCreated: 123 };
+      animalRepository.save({
+        animal: { id: 1, name: 'Animal', dateCreated: 123 },
+      });
 
-    animalRepository.save({
-      animal: animal1,
+      const returnValue = animalRepository.updateOne(
+        { id: 1234 },
+        { name: 'Animal' }
+      );
+
+      expect(returnValue).toEqual(null);
     });
 
-    const animal2 = { id: 2, name: 'Animal2', dateCreated: 123 };
+    it('updateOne should return null if name is undefined', () => {
+      const animalRepository = new AnimalRepository();
 
-    animalRepository.save({
-      animal: animal2,
+      animalRepository.save({
+        animal: { id: 1, name: 'Animal', dateCreated: 123 },
+      });
+
+      const returnValue = animalRepository.updateOne(
+        { id: 1 },
+        { name: undefined }
+      );
+
+      expect(returnValue).toEqual(null);
     });
 
-    const animal3 = { id: 3, name: 'Animal3', dateCreated: 123 };
+    it('updateOne should update animal and preserve order', () => {
+      const animalRepository = new AnimalRepository();
 
-    animalRepository.save({
-      animal: animal3,
+      const animal1 = { id: 1, name: 'Animal1', dateCreated: 123 };
+
+      animalRepository.save({
+        animal: animal1,
+      });
+
+      const animal2 = { id: 2, name: 'Animal2', dateCreated: 123 };
+
+      animalRepository.save({
+        animal: animal2,
+      });
+
+      const animal3 = { id: 3, name: 'Animal3', dateCreated: 123 };
+
+      animalRepository.save({
+        animal: animal3,
+      });
+
+      const newName = 'Animal2Changed';
+
+      animalRepository.updateOne({ id: animal2.id }, { name: newName });
+
+      expect(animalRepository.getAnimals()).toEqual([
+        animal1,
+        { ...animal2, name: newName },
+        animal3,
+      ]);
     });
-
-    const newName = 'Animal2Changed';
-
-    animalRepository.updateOne({ id: animal2.id }, { name: newName });
-
-    expect(animalRepository.getAnimals()).toEqual([
-      animal1,
-      { ...animal2, name: newName },
-      animal3,
-    ]);
   });
 
-  it('getAnimals should return empty array on repository instantiation', () => {
-    const animalRepository = new AnimalRepository();
+  describe('getAnimals', () => {
+    it('getAnimals should return empty array on repository instantiation', () => {
+      const animalRepository = new AnimalRepository();
 
-    expect(animalRepository.getAnimals()).toEqual([]);
+      expect(animalRepository.getAnimals()).toEqual([]);
+    });
   });
 });
